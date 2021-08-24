676dfc3b1bd46920d2b93a627454d5f704d8a3c6
UtkuOnDuty <70181510+UtkuOnDuty@users.noreply.github.com>
2021-08-24 22:45:59 +0300

    Update asdasdsdasadsadsad.lua

--- asdasdsdasadsadsad.lua
+++ asdasdsdasadsadsad.lua
@@ -36,11 +36,11 @@ local tableinsert = table.insert
 -- // Silent Aim Vars
 getgenv().SilentAim = {
     Enabled = true,
-    ShowFOV = false,
+    ShowFOV = true,
     FOVSides = 12,
     FOVColour = Color3fromRGB(255, 255, 255),
     VisibleCheck = true,
-    FOV = 30,
+    FOV = 40,
     HitChance = 100,
     Selected = LocalPlayer,
     SelectedPart = nil,
